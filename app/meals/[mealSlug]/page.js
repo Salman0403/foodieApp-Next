15fe35132789e09c7e0d7ba43da9bb6a48dde740
Salman0403 <muhammadsalman0403@gmail.com>
2024-12-11 17:57:45 +0500

    feat: meal details page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,36 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { getMeal } from "@/lib/meals";
 
-const MealsDetails =({params}) =>{
-    return(
-        <h1>Meals Details page {params.mealSlug}</h1>
-    )
-}
+const MealsDetails = ({ params }) => {
+  console.log("params: ", params);
+  const meal = getMeal(params.mealSlug);
 
-export default MealsDetails
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
+};
+
+export default MealsDetails;
